f08643daaebcb9387f80db23ad8945b6a8f0fe22
Annalisa <annalisa@annalisa-laptop.(none)>
2012-05-06 00:35:02 +0200

    added amazon_policy tag

--- lib/locomotive/liquid/tags/amazon_policy.rb
+++ lib/locomotive/liquid/tags/amazon_policy.rb
@@ -1,25 +1,41 @@
+require 'base64'
+#require 'liquid'
+
 module Liquid
   module Locomotive
     module Tags
       class AmazonPolicy < ::Liquid::Tag
 
-        Syntax = /(#{::Liquid::Expression}+)?/
+        #Syntax = /(#{::Liquid::Expression}+)?/
 
-        def initialize(tag_name, markup, tokens, context)
-          if markup =~ Syntax
-            @user = $1.gsub('\'', '')
-	    @redirect = $2.gsub('\'', '')
-          else
-            raise ::Liquid::SyntaxError.new("Syntax Error in 'amazon_policy' - Valid syntax: amazon_policy <user> <redirect_url>")
-          end
+        #def initialize(tag_name, markup, tokens, context)
+         # if markup =~ Syntax
+         #   @user = $1.gsub('\'', '')
+         # else
+         #   raise ::Liquid::SyntaxError.new("Syntax Error in 'amazon_policy' - Valid syntax: amazon_policy <user>")
+         # end
 
-          super
-        end
+         # super
+        #end
 
         def render(context)
-          %{
-          	<div></div>
-	   }
+	  date=Time.now.utc.strftime("%y-%m-%dT%H:%M:%SZ")
+	  bucket=ENV["UPLOADS_BUCKET"]
+	  redirect=ENV["REDIRECT_URL"]	
+	  user=context.registers[:current_locomotive_account].email
+
+          policy_document="""
+			{\"expiration\": \"#{date}\",
+  			 \"conditions\": [ 
+						{\"bucket\": \"#{bucket}\"}, 
+						[\"starts-with\", \"$key\", \"uploads/#{user}/\"],
+						{\"acl\": \"private\"},
+						{\"success_action_redirect\": \"#{redirect}\"},
+						[\"starts-with\", \"$Content-Type\", \"\"]
+  					]
+			}
+		"""
+	  #Base64.encode64(policy_document).gsub("\n","")
         end
       end
 
@@ -27,3 +43,5 @@ module Liquid
     end
   end
 end
+
+#Liquid::Template.parse('{%amazon_policy ciao%}').render(registers={:current_locomotive_account => {:email => "o"}})
